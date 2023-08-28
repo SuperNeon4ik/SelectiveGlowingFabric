4619763293c44ab23f748bd85fd0bdad411e6ef0
SuperNeon4ik <youtube@superneon4ik.me>
2023-08-28 23:49:05 +0300

    Glowing map

--- src/main/java/me/superneon4ik/selectiveglowing/SelectiveGlowing.java
+++ src/main/java/me/superneon4ik/selectiveglowing/SelectiveGlowing.java
@@ -6,9 +6,16 @@ import com.mojang.brigadier.Command;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SelectiveGlowing implements ModInitializer {
+    private static final Map<Integer, List<Integer>> GLOWING_MAP = new HashMap<>();
+
     /**
      * Runs the mod initializer.
      */
@@ -21,13 +28,23 @@ public class SelectiveGlowing implements ModInitializer {
                                 .executes(context -> {
                                     var target = EntityArgumentType.getPlayer(context, "target");
                                     var displayPlayers = EntityArgumentType.getPlayers(context, "displayplayers");
-                                    context.getSource().sendFeedback(Text.literal("Meow meow"), false);
+                                    GLOWING_MAP.put(target.getId(), displayPlayers.stream().map(Entity::getId).toList());
+                                    context.getSource().sendFeedback(Text.literal(String.format("Player %s is now glowing for %d players.",
+                                            target.getEntityName(), displayPlayers.size())), false);
                                     return Command.SINGLE_SUCCESS;
                                 }))
                         .then(literal("*reset")
                                 .executes(context -> {
                                     var target = EntityArgumentType.getPlayer(context, "target");
+                                    GLOWING_MAP.remove(target.getId());
+                                    context.getSource().sendFeedback(Text.literal("Removed glowing overrides for ")
+                                            .append(target.getName()), false);
                                     return Command.SINGLE_SUCCESS;
                                 })))));
+    }
+
+    public static boolean isGlowing(int targetId, int observerId) {
+        if (!GLOWING_MAP.containsKey(targetId)) return false;
+        return GLOWING_MAP.get(targetId).contains(observerId);
     }
 }
