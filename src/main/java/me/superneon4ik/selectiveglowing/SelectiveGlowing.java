af251fe2c9d9ceb94c9f64882bc305e864a37f5c
SuperNeon4ik <youtube@superneon4ik.me>
2023-08-28 20:52:21 +0300

    A "Hello world" mod

--- src/main/java/me/superneon4ik/selectiveglowing/SelectiveGlowing.java
+++ src/main/java/me/superneon4ik/selectiveglowing/SelectiveGlowing.java
@@ -1,6 +1,12 @@
 package me.superneon4ik.selectiveglowing;
 
+import static net.minecraft.server.command.CommandManager.*;
+
+import com.mojang.brigadier.Command;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.text.Text;
 
 public class SelectiveGlowing implements ModInitializer {
     /**
@@ -8,6 +14,13 @@ public class SelectiveGlowing implements ModInitializer {
      */
     @Override
     public void onInitialize() {
-
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("glow")
+                .requires(source -> source.hasPermissionLevel(4))
+                .then(argument("target", EntityArgumentType.player())
+                        .executes(context -> {
+                            var target = EntityArgumentType.getPlayer(context, "target");
+                            context.getSource().sendFeedback(Text.literal("Meow meow " + target.getName()), false);
+                            return Command.SINGLE_SUCCESS;
+                        }))));
     }
 }
