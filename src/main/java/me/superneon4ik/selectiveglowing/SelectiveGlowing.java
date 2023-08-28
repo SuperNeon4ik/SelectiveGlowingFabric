09081c96b70115ff0801f197aee189684cf41d8b
SuperNeon4ik <youtube@superneon4ik.me>
2023-08-28 17:40:22 +0300

    Initial setup

---
+++ src/main/java/me/superneon4ik/selectiveglowing/SelectiveGlowing.java
@@ -0,0 +1,13 @@
+package me.superneon4ik.selectiveglowing;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SelectiveGlowing implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/superneon4ik/selectiveglowing/client/SelectiveGlowingClient.java
@@ -0,0 +1,13 @@
+package me.superneon4ik.selectiveglowing.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SelectiveGlowingClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
